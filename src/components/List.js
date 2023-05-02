eaffa0e77d480ca57883eddfb87f911571463e71
Dilsher <dilsherb@gmail.com>
2023-05-02 09:23:57 +0300

    Layout setup for the project

---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Item from '../components/Item'
+
+const List = () => {
+  return (
+    <div>
+      Hello from list
+      <Item />
+    </div>
+  )
+}
+
+export default List
