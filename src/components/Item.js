12a3d60d180285adfc01260bb935582193483a1f
Dilsher <dilsherb@gmail.com>
2023-05-02 16:46:19 +0300

    js linter errors handled

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import {MdEdit, MdDelete} from 'react-icons/md'
+import React from 'react';
+import { MdEdit, MdDelete } from 'react-icons/md';
 
-const Item = ({item}) => {
-    const {id, name, amount} = item
+const Item = ({ item }) => {
+  const { id, name, amount } = item;
   return (
-        <tr>
-            <td>{name}</td>
-            <td>{amount}</td>
-            <td><MdEdit  className='text-success'/></td>
-            <td><MdDelete  className='text-danger'/></td>
-        </tr>
-  )
-}
+    <tr>
+      <td>{name}</td>
+      <td>{amount}</td>
+      <td><MdEdit className="text-success" /></td>
+      <td><MdDelete className="text-danger" /></td>
+    </tr>
+  );
+};
 
-export default Item
+export default Item;
--- src/components/List.js
+++ src/components/List.js
@@ -1,44 +1,47 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md';
 import Item from './Item';
-import {MdDelete} from 'react-icons/md'
 
-const list = ({list}) => {
-  return (
-    <>
-    {list.length>0 ?
-    <div>
-        <table 
-        class="container 
-                table 
-                table-success 
-                table-hover 
-                rounded 
-                rounded-3 
+const list = ({ list }) => (
+  <>
+    {list.length > 0
+      ? (
+        <div>
+          <table
+            className="container
+                table
+                table-success
+                table-hover
+                rounded
+                rounded-3
                 overflow-hidden
                 table-borderless
-                table-striped">
-                    <thead>
-                        <tr className='table-primary text-success'>
-                            <th>Expense</th>
-                            <th>Amount (PKR)</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
+                table-striped"
+          >
+            <thead>
+              <tr className="table-primary text-success">
+                <th>Expense</th>
+                <th>Amount (PKR)</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
             <tbody>
-                {list.map(item=>{return <Item key={item.id} item={item}/>})}
+              {list.map((item) => <Item key={item.id} item={item} />)}
             </tbody>
-        </table>
-      <button className='btn btn-danger'>
-        Clear All
-        <MdDelete className='btn-icon'/>
-      </button>
-      </div>
-      : <div className='container bg-secondary text-info fs-2 py-3 my-3'>
-            There is nothing to show. Please add items to see your budget
-        </div>}
-    </>
-  )
-}
+          </table>
+          <button className="btn btn-danger">
+            Clear All
+            <MdDelete className="btn-icon" />
+          </button>
+        </div>
+      )
+      : (
+        <div className="container bg-secondary text-info fs-2 py-3 my-3">
+          There is nothing to show. Please add items to see your budget
+        </div>
+      )}
+  </>
+);
 
-export default list
+export default list;
