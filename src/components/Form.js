aac175f81cfa46fb1bda986c0828d309df38078a
Dilsher <dilsherb@gmail.com>
2023-05-03 11:17:49 +0300

    form completed without css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { MdSend } from 'react-icons/md';
 
-const Farm = () => (
-  <div>
-    Hello from Farm
-  </div>
+const Form = () => (
+  <form className="container">
+    <div className="form-center">
+      <div className="form-group">
+        <label htmlFor="expense">
+          Expense
+          <input type="text" placeholder="e.g. rent" className="form-control" id="expense" name="expense" />
+        </label>
+      </div>
+      <div className="form-group">
+        <label htmlFor="amount">
+          Amount
+          <input type="text" placeholder="e.g. 500" className="form-control" id="amount" name="amount" />
+        </label>
+      </div>
+      <button type="submit" className="btn btn-success">
+        Submit <MdSend />
+      </button>
+    </div>
+  </form>
 );
 
-export default Farm;
+export default Form;
